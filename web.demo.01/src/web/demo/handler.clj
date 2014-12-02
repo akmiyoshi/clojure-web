c85244f35f018f9c57a1f3c060c9a1a7b70fc86c
akmiyoshi <akio.miyoshi@gmail.com>
2014-12-03 02:10:31 +0900

    .

--- web.demo.01/src/web/demo/handler.clj
+++ web.demo.01/src/web/demo/handler.clj
@@ -3,14 +3,43 @@
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
             [compojure.handler :as handler]
-            [hiccup.middleware :as hiccup]
-            [ring.util.response :as ring]))
+            [ring.middleware.content-type :as content-type]
+            [ring.util.response :as response]))
+
+(def call-count (atom 0))
+
+(defn- log-request
+  [$request]
+  (swap! call-count inc)
+  (println "[ACCESS] " (:request-method $request) (:uri $request) @call-count "回目" (str $request))
+  ;(println (str $request))
+  nil)
 
 (defroutes app-routes
-  (GET "/" [] "Hello World こんにちは 世界")
+  (ANY "/*" $request (log-request $request) nil)
+  (GET "/" [] (str "Hello World こんにちは 世界 " (new java.util.Date)))
+  (GET "/*.html" $request
+       (println "Accessing /*.html ..." (:uri $request))
+       (println (str $request))
+       (let [$res (response/resource-response (:uri $request) {:root "public"})]
+         (when $res (response/content-type $res "text/html;charset=UTF-8"))))
+  (GET "/ih" []
+       (println "Accessing /ih ...")
+       (response/resource-response "/index.html" {:root "public"}))
+  (GET "/ip" [] (-> (response/resource-response "/index.page" {:root "public"})
+                  (response/content-type "text/html;charset=UTF-8")
+                  ))
+  (GET "/cp1" [] (clostache-page-1))
+  (GET "/cp2" [] (clostache-page-2))
+  (GET "/tp" [] (table-page))
+  (GET "/mt" [] (main-template))
+  (GET "/video/:id" [id] (println id) (str "id2=" id))
+  (GET "/user/:user" [user x] (println user) (str " user=" user " x=" x))
   (route/resources "/")
   (route/not-found "Not Found"))
 
 (def app
-  (-> (handler/site app-routes)
-      (hiccup/wrap-base-url)))
+  (-> 
+    (handler/site app-routes)
+    (content-type/wrap-content-type
+      {:mime-types {"page" "text/html;charset=UTF-8"}})))
